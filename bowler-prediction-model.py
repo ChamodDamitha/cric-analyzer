9f0d73ac2f7851781257359cc442472408844e42
ChamodDamitha <chamod.14@cse.mrt.ac.lk>
2018-04-20 23:10:54 +0530

    Fix issue of output variable - accuracy = 0.2

--- bowler-prediction-model.py
+++ bowler-prediction-model.py
@@ -8,19 +8,21 @@ data = pd.read_csv('Samples/Dismissals/all_dismissals.csv', error_bad_lines=Fals
 #One hot encoding categoricals
 # data = pd.get_dummies(data)
 
-
 predictors = ['batting-hand', 'innings', 'batsman_runs', 'batsman_balls',
               'batsman_4s', 'batsman_6s', 'batting_position', 'dismissed_ball', 'wickets_fallen', 'runs_scored']
 
-targets = ['way_out', 'bowler_type']
+targets = ['bowler_type_way_out']
 
 X = data[predictors]
 y = data[targets]
 X = pd.get_dummies(X)
 y = pd.get_dummies(y)
 
+print(y.columns)
+
 from sklearn.model_selection import train_test_split
-train_X, test_X, train_y, test_y = train_test_split(X.as_matrix(), y.as_matrix(), test_size=0.25)
+train_X, test_X, train_y, test_y = train_test_split(X.as_matrix(), y.as_matrix(), test_size=0.2)
+train_X, val_X, train_y, val_y = train_test_split(train_X, train_y, test_size=0.2)
 
 print(train_X.shape)
 print(train_y.shape)
@@ -31,9 +33,9 @@ print(test_y.shape)
 model = Sequential()
 model.add(Dense(8, input_dim=train_X.shape[1], activation='relu'))
 model.add(Dropout(0.25))
-model.add(Dense(3, activation='relu'))
-model.add(Dropout(0.25))
-model.add(Dense(7, activation='relu'))
+model.add(Dense(15, activation='relu'))
+# model.add(Dropout(0.25))
+# model.add(Dense(5, activation='relu'))
 model.add(Dense(train_y.shape[1], activation='softmax'))
 #
 # compile model
@@ -43,14 +45,24 @@ model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accur
 print(model.summary())
 history = model.fit(
     train_X, train_y,
-    validation_data=(test_X, test_y),
-    nb_epoch=10,
+    validation_data=(val_X, val_y),
+    nb_epoch=20,
     batch_size=64,
     callbacks=[earlyStoper]
 )
 
+# NN model testing
+loss, acc = model.test_on_batch(test_X, test_y)
+print('Test loss : ' + str(loss))
+print('Test accuracy : ' + str(acc))
+
+# sample prediction
+train_X = pd.DataFrame(train_X)
+print("sample X : " + str(train_X.loc[[0]]))
+print("predicted y : " + str(model.predict(train_X.loc[[0]])))
+
 # list all data in history
-print(history.history.keys())
+# print(history.history.keys())
 # summarize history for accuracy
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
--- csv_generator.py
+++ csv_generator.py
@@ -3,7 +3,7 @@ from pprint import pprint
 import glob
 
 with open('Samples/Dismissals/all_dismissals.csv', 'wb') as file:
-    line = 'batting-hand,bowler_type,innings,way_out,' \
+    line = 'batting-hand,bowler_type_way_out,innings,' \
            'batsman_runs,batsman_balls,batsman_4s,batsman_6s,batting_position,' \
            'dismissed_ball,wickets_fallen,runs_scored'
     file.write(line)
@@ -16,8 +16,8 @@ for filename in glob.glob('Samples/Dismissals/*.json'):
         for item in data:
             if ('bowler' in item) and item['bowler'] and ('ball' in item) and ('scoreAt' in item):
                 scoreAt = item['scoreAt'].split('/')
-                line = item['batsman']['batting-hand'] + ',' + item['bowler']['type'] + ',' + \
-                       item['team_innings'] + ',' + item['dismissal']['wayOut'] + ',' + \
+                line = item['batsman']['batting-hand'] + ',' + item['bowler']['type'] + '_' + item['dismissal']['wayOut']\
+                       + ',' + item['team_innings'] + ',' + \
                        item['player_innings']['runs'] + ',' + item['player_innings']['balls'] \
                        + ',' + item['player_innings']['4s'] + ',' + item['player_innings']['6s'] \
                        + ',' + item['player_innings']['batting_position'] + ',' + item['ball'] \
